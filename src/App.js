9e9791767e5d6c99671e98267a472221da930aae
AbdelkaderBelhadj-code <belhajabdelkader3@gmail.com>
2023-08-28 11:35:21 +0100

    header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="bg-red-500">
-      React App 
+    <div className="">
+      <Header/>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import logo from "../assest/logo.png";
+import { Link } from "react-router-dom";
+import {FaUserAlt} from "react-icons/fa"
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <header className="fixed shadow-lg w-full h-16 px-2 md:px-4">
+      <div className="flex items-center h-full">
+        <Link to={""}>
+          <div className="h-12">
+            <img src={logo} className="h-full" />
+          </div>
+        </Link>
+
+        <div className="">
+            <nav>
+
+            </nav>
+            <div className="">
+              <FaUserAlt />
+            </div>
+        </div>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
